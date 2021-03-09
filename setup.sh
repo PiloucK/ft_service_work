15fee23d9dd6d1597f97e0dd86e731243e02c69d
PiloucK <clkuznie@student.42.fr>
2021-03-09 12:01:28 +0100

    first bash commands setup. generate a cluster and get its ipaddress to replace the variables in config files. set metallb through installation by manifest. generate metallb secrets and apply the configmap from project sources

--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml
-# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+minikube start --vm-driver=docker
 
-# Setup the namespace 'monitoring' for clarity in big teams To delete after testing
-minikube start
+# Get the cluster's IP address and store it in a variable
+CORRECT_CLUSTER_IP=$(kubectl get node -o wide | grep -oP  '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
 
-kubectl create namespace monitoring
+# Replace the variable name in all files of the project by its value
+sed -i -s "s/CORRECT_CLUSTER_IP/${CORRECT_CLUSTER_IP}/g" srcs/*/*
 
-kubectl create secret generic influxdb-creds \
-  --from-literal=INFLUXDB_DATABASE=local_monitoring \
-  --from-literal=INFLUXDB_USERNAME=clkuznie \
-  --from-literal=INFLUXDB_PASSWORD=toto \
-  --from-literal=INFLUXDB_HOST=influxdb
+# Metallb
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/metallb/metallb-config-map.yaml
 
-kubectl expose deployment influxdb --port=8086 --target-port=8086 --protocol=TCP --type=ClusterIP
+# kubectl create namespace monitoring
+
+# kubectl create secret generic influxdb-creds \
+#   --from-literal=INFLUXDB_DATABASE=local_monitoring \
+#   --from-literal=INFLUXDB_USERNAME=clkuznie \
+#   --from-literal=INFLUXDB_PASSWORD=toto \
+#   --from-literal=INFLUXDB_HOST=influxdb
+
+# kubectl expose deployment influxdb --port=8086 --target-port=8086 --protocol=TCP --type=ClusterIP
