1d993061145552c1fe2a64133c98795577cc9367
PiloucK <clkuznie@student.42.fr>
2021-03-24 17:29:29 +0100

    basic nginx setup working through single ip

--- srcs/mysql/startup.sh
+++ srcs/mysql/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/mysql_install_db --user=mysql --datadir=/var/lib/mysql
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-/usr/bin/mysqld_safe
+mysqld_safe
--- startup.sh
+++ startup.sh
@@ -12,6 +12,15 @@ CORRECT_CLUSTER_IP=$(kubectl get node -o wide | grep -oP  '[[:digit:]]{1,3}\.[[:
 # Replace the variable name in all files of the project by its value
 sed -i -E "s/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/${CORRECT_CLUSTER_IP}/g" srcs/*/* > /dev/null
 
+
+# Create same certificates for all nginx using services
+openssl dhparam -out srcs/tmp_cert/dh2048.pem 2048
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout srcs/tmp_cert/ft_service.key -out srcs/tmp_cert/ft_service.crt -subj "/C=FR/ST=IDE/L=Paris/O=42/OU=42/CN=ft_service.com"
+cp srcs/tmp_cert/* srcs/nginx/cert
+cp srcs/tmp_cert/* srcs/wordpress/cert
+rm -f srcs/tmp_cert/*
+
+
 # echo "Addons loading\n"
 # touch addons_trace
 # minikube addons enable metrics-server >> addons_trace
@@ -20,9 +29,20 @@ sed -i -E "s/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/${CORRECT_CLUSTER_IP
 # minikube addons enable metrics-server >> addons_trace
 # rm addons_trace
 
+
 # Metallb
 echo "Metallb loading\n"
 # touch metallb_trace
+# see what changes would be made, returns nonzero returncode if different
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+
+# actually apply the changes, returns nonzero returncode on errors only
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 #  >> metallb_trace
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
@@ -34,12 +54,16 @@ kubectl apply -f srcs/metallb/metallb-config-map.yaml
 #  >> metallb_trace
 # rm metallb_trace
 
-# # Mysql
+
+# Mysql
 # echo "Mysql loading\n"
 # touch mysql_trace
-# docker build -t mysql srcs/mysql/ >> mysql_trace
-# kubectl apply -f srcs/mysql/mysql.yaml >> mysql_trace
+docker build -t mysql srcs/mysql/
+#  >> mysql_trace
+kubectl apply -f srcs/mysql/mysql.yaml
+#  >> mysql_trace
 # rm mysql_trace
+
 
 # Nginx
 echo "Nginx loading\n"
@@ -49,6 +73,12 @@ docker build -t nginx srcs/nginx/
 kubectl apply -f srcs/nginx/nginx.yaml
 #  >> nginx_trace
 # rm nginx_trace
+
+
+# Wordpress
+docker build -t wordpress srcs/wordpress
+kubectl apply -f srcs/wordpress/wordpress.yaml
+
 
 echo ${CORRECT_CLUSTER_IP}
 # kubectl create namespace monitoring
