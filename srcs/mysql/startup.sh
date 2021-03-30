6e23ebdb83b3652ea33c4a1bf87395e85ce826ff
PiloucK <clkuznie@student.42.fr>
2021-03-30 17:55:33 +0200

    nginx wordpress mysql working together allowing testing for all other services, phpmyadmin in progress, question on usefulness of .php location in wordpress nginx conf

--- srcs/mysql/startup.sh
+++ srcs/mysql/startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mysql_install_db
+mysql_install_db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
 
-mysqld_safe &
+mysqld_safe --datadir=/var/lib/mysql &
 
 sleep 2
 
 mysql -u root << EOF
 CREATE USER 'ftservice'@'%' IDENTIFIED BY 'ftservice';
 CREATE DATABASE IF NOT EXISTS wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'ftservice'@'%';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'ftservice'@'%' IDENTIFIED BY 'ftservice';
 FLUSH PRIVILEGES;
 EOF
 
